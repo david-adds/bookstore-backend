747e81ce351d61a6520dcac7efa3ac58e1ba5592
david-adds <55636118+david-adds@users.noreply.github.com>
2024-01-01 23:20:19 -0300

    validate against empty spaces as well

--- src/models/globalValidator.js
+++ src/models/globalValidator.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 mongoose.Schema.Types.String.set("validate", {
-  validator: (value) => value !== "",
+  validator: (value) => value.trim() !== "",
   message: ({ path }) => `The field \'${path}\' is blank.`
 });
